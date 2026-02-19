16f8efeab3bcd0daa70f03f75f197dfafc20796e
artoo-claw <artoo-claw@users.noreply.github.com>
2026-02-19 13:25:26 +0000

    fix: footer logo stretching + unified social icons in #66FFFC
    - Logo: object-fit contain, proper aspect ratio
    - Discord & Twitter/X: replaced with inline SVGs using currentColor
    - All social icons now inherit #66FFFC from SocialLink

--- src/components/About/AboutFooter.tsx
+++ src/components/About/AboutFooter.tsx
@@ -6,7 +6,6 @@ import { BREAKPOINTS } from 'theme'
 import { ExternalLink, StyledRouterLink } from 'theme/components'
 import { useIsDarkMode } from 'theme/components/ThemeToggle'
 
-import { DiscordIcon, TwitterIcon } from './Icons'
 import lightswapLogo from 'assets/lightswap-logo.png'
 
 const Footer = styled.div`
@@ -44,8 +43,9 @@ const LogoSectionBottom = styled(LogoSection)`
 `
 
 const StyledLogo = styled.img`
-  height: 40px;
+  height: 36px;
   width: auto;
+  object-fit: contain;
   display: none;
 
   @media screen and (min-width: ${BREAKPOINTS.lg}px) {
@@ -62,6 +62,7 @@ const SocialLinks = styled.div`
 
 const SocialLink = styled.a`
   display: flex;
+  color: #66FFFC;
 `
 
 const FooterLinks = styled.div`
@@ -110,6 +111,18 @@ const Copyright = styled.span`
   color: ${({ theme }) => theme.neutral3};
 `
 
+const DiscordIconCustom = ({ size = 32 }: { size?: number }) => (
+  <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M20.317 4.492C18.787 3.786 17.147 3.266 15.432 2.966C15.219 3.348 14.974 3.862 14.803 4.268C12.972 3.988 11.159 3.988 9.363 4.268C9.192 3.862 8.942 3.348 8.727 2.966C7.01 3.266 5.368 3.788 3.839 4.496C0.553 9.415 -0.339 14.21 0.107 18.939C2.167 20.464 4.163 21.38 6.123 21.988C6.607 21.332 7.037 20.634 7.408 19.9C6.699 19.636 6.019 19.31 5.375 18.932C5.546 18.808 5.713 18.678 5.875 18.544C10.144 20.524 14.792 20.524 19.013 18.544C19.177 18.678 19.344 18.808 19.513 18.932C18.867 19.312 18.185 19.638 17.475 19.902C17.846 20.634 18.274 21.334 18.76 21.99C20.722 21.382 22.72 20.466 24.781 18.939C25.305 13.44 23.901 8.692 20.317 4.492ZM8.352 16.032C7.116 16.032 6.107 14.9 6.107 13.518C6.107 12.136 7.095 11.002 8.352 11.002C9.608 11.002 10.618 12.134 10.596 13.518C10.598 14.9 9.608 16.032 8.352 16.032ZM16.536 16.032C15.3 16.032 14.291 14.9 14.291 13.518C14.291 12.136 15.279 11.002 16.536 11.002C17.792 11.002 18.802 12.134 18.78 13.518C18.78 14.9 17.792 16.032 16.536 16.032Z" fill="currentColor" transform="scale(0.92) translate(1, 1)"/>
+  </svg>
+)
+
+const XIcon = ({ size = 32 }: { size?: number }) => (
+  <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M18.244 2.25H21.552L14.325 10.51L22.827 21.75H16.17L10.956 14.933L4.99 21.75H1.68L9.41 12.915L1.254 2.25H8.08L12.793 8.481L18.244 2.25ZM17.083 19.77H18.916L7.084 4.126H5.117L17.083 19.77Z" fill="currentColor"/>
+  </svg>
+)
+
 const EmailIcon = ({ size = 32 }: { size?: number }) => (
   <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
@@ -140,10 +153,10 @@ const LogoSectionContent = () => {
           <EmailIcon size={28} />
         </SocialLink>
         <SocialLink href="https://discord.com/invite/lightlinkchain" target="_blank" rel="noopener noreferrer">
-          <DiscordIcon size={28} />
+          <DiscordIconCustom size={28} />
         </SocialLink>
         <SocialLink href="https://x.com/lightlinkchain" target="_blank" rel="noopener noreferrer">
-          <TwitterIcon size={28} />
+          <XIcon size={28} />
         </SocialLink>
         <SocialLink href="https://www.linkedin.com/company/lightlinkchain" target="_blank" rel="noopener noreferrer">
           <LinkedInIcon size={28} />
