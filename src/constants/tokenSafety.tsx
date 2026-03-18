52a64ddff1deffa7aa4998908a20d42420be5b0b
danenright <denright@gmail.com>
2026-03-18 16:28:38 +1100

    fix(tokens): mark LightLink tokens as safe, rebrand safety warnings
    Add all LightLink COMMON_BASES tokens (WETH, USDC, USDC.e, USDT, LL)
    to the token safety lookup as UNI_DEFAULT so they don't trigger the
    'Warning' popup when selected in the swap token picker. These tokens
    are the core supported tokens on the exchange and should not require
    user confirmation.
    
    Also replace Uniswap references in token safety warning copy with
    StellaSwap, and update the safety article link to LightLink docs.

--- src/constants/tokenSafety.tsx
+++ src/constants/tokenSafety.tsx
@@ -6,7 +6,7 @@ import { ZERO_ADDRESS } from './misc'
 import { NATIVE_CHAIN_ID } from './tokens'
 import tokenSafetyLookup, { TOKEN_LIST_TYPES } from './tokenSafetyLookup'
 
-export const TOKEN_SAFETY_ARTICLE = 'https://support.uniswap.org/hc/en-us/articles/8723118437133'
+export const TOKEN_SAFETY_ARTICLE = 'https://docs.lightlink.io/'
 
 export enum WARNING_LEVEL {
   MEDIUM,
@@ -33,8 +33,8 @@ export function getWarningCopy(warning: Warning | null, plural = false) {
         heading = (
           <Plural
             value={plural ? 2 : 1}
-            _1="This token isn't traded on leading U.S. centralized exchanges or frequently swapped on Uniswap."
-            other="These tokens aren't traded on leading U.S. centralized exchanges or frequently swapped on Uniswap."
+            _1="This token isn't traded on leading U.S. centralized exchanges or frequently swapped on StellaSwap."
+            other="These tokens aren't traded on leading U.S. centralized exchanges or frequently swapped on StellaSwap."
           />
         )
         description = <Trans>Always conduct your own research before trading.</Trans>
@@ -43,8 +43,8 @@ export function getWarningCopy(warning: Warning | null, plural = false) {
         description = (
           <Plural
             value={plural ? 2 : 1}
-            _1="You can't trade this token using the Uniswap App."
-            other="You can't trade these tokens using the Uniswap App."
+            _1="You can't trade this token using StellaSwap."
+            other="You can't trade these tokens using StellaSwap."
           />
         )
         break
